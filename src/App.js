73525b58d814579c9fd98bda071110f45a9267d3
Day SY <69306872+Day-101@users.noreply.github.com>
2023-04-11 21:27:44 +0200

    ✨ fetch Weather Data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import axios from "axios";
 import "leaflet/dist/leaflet.css";
 import cities from "./cities";
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  const fetchWeatherData = async (lat, lon) => {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=539a92a71fbb1b6ee46f8afdfc95bb2e`
+    );
+    setWeatherData(response.data);
+  };
+
   return (
     <div className="App">
       <MapContainer
@@ -15,7 +26,15 @@ function App() {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {cities.map((city, index) => (
-          <Marker key={index} position={[city.lat, city.lon]}>
+          <Marker
+            key={index}
+            position={[city.lat, city.lon]}
+            eventHandlers={{
+              click: () => {
+                fetchWeatherData(city.lat, city.lon);
+              },
+            }}
+          >
             <Popup>{city.name}</Popup>
           </Marker>
         ))}
