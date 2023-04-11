3b5e43311740bfd37440e4a66e96eb363a28b4f3
Day SY <69306872+Day-101@users.noreply.github.com>
2023-04-11 21:21:25 +0200

    🍱 add cities datas

---
+++ src/cities.js
@@ -0,0 +1,19 @@
+const cities = [
+  {
+    name: "Paris",
+    lat: 48.8566,
+    lon: 2.3522,
+  },
+  {
+    name: "New York",
+    lat: 40.7128,
+    lon: -74.0060,
+  },
+  {
+    name: "Tokyo",
+    lat: 35.6895,
+    lon: 139.6917,
+  },
+];
+
+export default cities;
